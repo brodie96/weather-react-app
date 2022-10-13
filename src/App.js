d843405589fd476dc65b556d505ff3fbaeae08f8
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-10-13 12:50:48 +0200

    added source code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
       <header className="App-header">
         <h1>Weather Search</h1>
         <Search />
+        <p>
+          <a
+            href="https://github.com/brodie96/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced code
+          </a>{" "}
+          <br />
+          by Rebecca Brodie
+        </p>
       </header>
     </div>
   );
